7290f8bbc26e807265e44575ddacdef58c236fe1
Wonyoungpark <holly31521@gmail.com>
2021-01-26 15:25:54 +0900

    add nodejs files

---
+++ Nodejs/Nodejs-master/main.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+const bodyParser = require('body-parser');
+var compression = require('compression');
+var helmet = require('helmet');
+app.use(helmet());
+
+var indexRouter = require("./routes/index");
+var topicRouter = require('./routes/topic');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(compression());
+app.get('*', function(request, response, next){
+  fs.readdir("./data", function (error, filelist){
+    request.list = filelist;
+    next();
+  });
+});
+
+app.use('/',indexRouter);
+app.use('/topic',topicRouter);
+
+app.use(function(request, response, next){
+  response.status(404).send('Sorry cant find that!');
+});
+
+app.use(function(err, request, response, next){
+  console.error(err.stack);
+  response.status(500).send('Something broke!');
+})
+
+// app.listen(3000,() => console.log('Example app listening on port 3000!'))
+app.listen(3000, function(){
+  console.log("Example app listening on port 3000!")
+});
