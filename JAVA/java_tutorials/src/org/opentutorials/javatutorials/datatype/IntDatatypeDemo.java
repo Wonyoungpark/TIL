a1a08952b7f8ad066d49ca74cd2ab4ef16e398df
Wonyoungpark <holly31521@gmail.com>
2021-01-31 23:13:55 +0900

    add java files

---
+++ JAVA/java_tutorials/src/org/opentutorials/javatutorials/datatype/IntDatatypeDemo.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.datatype;
+
+public class IntDatatypeDemo {
+
+	public static void main(String[] args) {
+		byte a;
+		a = 127;
+		
+		short b;
+		b = 128;
+	}
+
+}
