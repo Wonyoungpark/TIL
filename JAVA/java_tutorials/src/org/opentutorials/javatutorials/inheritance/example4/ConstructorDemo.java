12df5ae3c180ed455af27084c214614534828f7f
Wonyoungpark <holly31521@gmail.com>
2021-05-08 14:47:02 +0900

    add java files

---
+++ JAVA/java_tutorials/src/org/opentutorials/javatutorials/inheritance/example4/ConstructorDemo.java
@@ -0,0 +1,9 @@
+package org.opentutorials.javatutorials.inheritance.example4;
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		ConstructorDemo c = new ConstructorDemo();
+	}
+
+}
