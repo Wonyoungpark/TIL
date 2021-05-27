9d24fcf8076776bc05eafc59b806cdf8fc0152e4
Wonyoungpark <holly31521@gmail.com>
2021-05-27 12:36:29 +0900

    add java files

--- JAVA/java_tutorials/src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
+++ JAVA/java_tutorials/src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
@@ -4,10 +4,16 @@ class Calculator{
 	int left, right;
 	
 	public void setOprands(int left, int right) {
+//		if (right == 0) {
+//			throw new IllegalArgumentException("두번째 인자는 0을 혀용하지 않습니다.");
+//		}
 		this.left = left;
 		this.right = right;
 	}
 	public void divide() {
+		if (right == 0) {
+			throw new ArithmeticException("0으로 나눌 수 없습니다.");
+		}
 		try { // 예외 처리
 			System.out.print("계산결과는 ");
 			System.out.print(this.left/this.right);
@@ -26,7 +32,11 @@ public class CalculatorDemo {
 	public static void main(String[] args) {
 		Calculator c1 = new Calculator();
 		c1.setOprands(10, 0);
-		c1.divide();
+		try {
+			c1.divide();
+		} catch(ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
