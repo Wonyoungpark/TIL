eae6606fb7eda8e57e17d00d2052343dde30b894
Wonyoungpark <holly31521@gmail.com>
2021-06-11 11:28:49 +0900

    add daily java

---
+++ JAVA/java_study/src/daily/codeup/java_study/No1017.java
@@ -0,0 +1,14 @@
+package daily.codeup.java_study;
+
+import java.util.Scanner;
+
+public class No1017 {
+
+	public static void main(String[] args) {
+		int a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        System.out.format("%d %d %d",a,a,a);
+	}
+
+}
---
+++ JAVA/java_study/src/daily/codeup/java_study/No1018.java
@@ -0,0 +1,13 @@
+package daily.codeup.java_study;
+
+import java.util.Scanner;
+
+public class No1018 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		System.out.printf("%s", a);
+	}
+
+}
