1dce04d83c5914ac29207bd864fe24b32c65d3c2
Wonyoungpark <holly31521@gmail.com>
2021-06-14 16:15:24 +0900

    add java programming

---
+++ JAVA/java_study/src/daily/codeup/java_study/No1022.java
@@ -0,0 +1,14 @@
+package daily.codeup.java_study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class No1022 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine());
+	}
+
+}
