c3c371fc778bd9ee19b9ba98bcb5740c0aad024f
Wonyoungpark <holly31521@gmail.com>
2021-06-25 14:19:07 +0900

    add daily programming

--- JAVA/java_study/src/Codeup/daily/java_study/No1025.java
+++ JAVA/java_study/src/Codeup/daily/java_study/No1025.java
@@ -1,10 +1,24 @@
 package Codeup.daily.java_study;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class No1025 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str[]  = br.readLine().split("");
+		int cnt = 5;
+		
+		for (String s: str) {
+			cnt--;
+			System.out.printf("["+s);
+			for (int i = 0; i<cnt; i++) {
+				System.out.printf("0");
+			}
+			System.out.printf("]\n");
+		}
 	}
 
 }
