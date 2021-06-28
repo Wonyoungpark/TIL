cb679363a7dff8db9f7267745b0ac07d89a1ba4a
Wonyoungpark <holly31521@gmail.com>
2021-06-28 15:40:34 +0900

    add daily codeup

---
+++ JAVA/java_study/src/Codeup/daily/java_study/No1030.java
@@ -0,0 +1,13 @@
+package Codeup.daily.java_study;
+
+import java.util.Scanner;
+
+public class No1030 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long num = sc.nextLong();
+		System.out.printf("%d",num);
+	}
+
+}
