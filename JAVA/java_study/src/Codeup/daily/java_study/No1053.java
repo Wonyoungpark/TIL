8cfd24f0521f36537ad588761ff1de5a6141d2cd
Wonyoungpark <holly31521@gmail.com>
2021-07-06 11:40:12 +0900

    add daily codeup

---
+++ JAVA/java_study/src/Codeup/daily/java_study/No1053.java
@@ -0,0 +1,13 @@
+package Codeup.daily.java_study;
+
+import java.util.Scanner;
+
+public class No1053 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(a == 1? 0:1);
+	}
+
+}
