f0f2f20b6d0eff8c6182c924038a651d6fe62c31
Wonyoungpark <holly31521@gmail.com>
2021-09-18 11:35:38 +0900

    add daily java

---
+++ JAVA/java_study/src/Codeup/daily/java_study/No1083.java
@@ -0,0 +1,19 @@
+package Codeup.daily.java_study;
+
+import java.util.Scanner;
+
+public class No1083 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			if (i%3==0) {
+				System.out.printf("X ");
+			} else {
+				System.out.printf("%d ",i);
+			}
+		}
+	}
+
+}
