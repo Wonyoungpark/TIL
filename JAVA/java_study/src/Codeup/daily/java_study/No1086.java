9f3b0ee4e8153cccbbf704441b37ad916e822d02
Wonyoungpark <holly31521@gmail.com>
2021-10-20 15:15:31 +0900

    add daiy java

--- JAVA/java_study/src/Codeup/daily/java_study/No1086.java
+++ JAVA/java_study/src/Codeup/daily/java_study/No1086.java
@@ -2,16 +2,15 @@ package Codeup.daily.java_study;
 
 import java.util.Scanner;
 
-public class No1085 {
+public class No1086 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		double storage = 1;
-		
-		for (int i=0;i<4;i++) {
+		for(int i=0;i<3;i++) {
 			storage *= sc.nextDouble();
 		}
-		System.out.printf("%.1f MB",storage/Math.pow(2, 23));
+		System.out.printf("%.2f MB",storage/Math.pow(2, 23));
 	}
 
 }
