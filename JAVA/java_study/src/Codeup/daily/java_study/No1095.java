611094e76bfc0dc318b5fce02488a7989e645a60
Wonyoungpark <holly31521@gmail.com>
2021-10-25 13:46:30 +0900

    add daily java

---
+++ JAVA/java_study/src/Codeup/daily/java_study/No1095.java
@@ -0,0 +1,19 @@
+package Codeup.daily.java_study;
+
+import java.util.Scanner;
+
+public class No1095 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int mini=n;
+		
+		for(int i=0;i<n;i++) {
+			int a = sc.nextInt();
+			if(a<mini) mini=a;
+		}
+		System.out.print(mini);
+	}
+
+}
