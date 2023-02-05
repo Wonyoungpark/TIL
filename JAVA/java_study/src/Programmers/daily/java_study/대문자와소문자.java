66f14355c4c0cc0dd6d774ae72d8834a2799dd50
Wonyoungpark <holly31521@gmail.com>
2023-02-05 23:21:59 +0900

    programmers

---
+++ JAVA/java_study/src/Programmers/daily/java_study/대문자와소문자.java
@@ -0,0 +1,26 @@
+package Programmers.daily.java_study;
+
+class Solution1 {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(char a : my_string.toCharArray()){
+            if (Character.isUpperCase(a)){
+                answer += Character.toLowerCase(a);
+            } else {
+                answer += Character.toUpperCase(a);
+            }
+        };
+        return answer;
+    }
+}
+
+public class 대문자와소문자 {
+
+	public static void main(String[] args) {
+		Solution1 s = new Solution1();
+		System.out.println(s.solution("cccCCC"));
+		System.out.println(s.solution("abCdEfghIJ"));
+	}
+
+}
