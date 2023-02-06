60f1829c63313ea60a255d7a73394da464b7e136
Wonyoungpark <holly31521@gmail.com>
2023-02-06 12:42:41 +0900

    programmers

---
+++ JAVA/java_study/src/Programmers/daily/java_study/가까운수.java
@@ -0,0 +1,30 @@
+package Programmers.daily.java_study;
+
+class Solution2 {
+    public int solution(int[] array, int n) {
+        int answer = array[0];
+        int num = Math.abs(array[0]-n);
+        
+        for (int a : array){
+            if (num>Math.abs(a-n)){
+                num = Math.abs(a-n);
+                answer = a;
+            } else if (num == Math.abs(a-n)){
+                answer = Math.min(answer,a);
+            }
+        }
+        return answer;
+    }
+}
+
+public class 가까운수 {
+
+	public static void main(String[] args) {
+		Solution2 s = new Solution2();
+		int[] arr = {3,10,28};
+		System.out.println(s.solution(arr, 20));
+		int[] arr1 = {10, 11,12};
+		System.out.println(s.solution(arr1, 13));
+	}
+
+}
---
+++ JAVA/java_study/src/Programmers/daily/java_study/암호해독.java
@@ -0,0 +1,23 @@
+package Programmers.daily.java_study;
+
+class Solution3 {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        for(int i=code-1; i<cipher.length(); i += code){
+            answer += cipher.charAt(i);
+        }
+        return answer;
+    }
+}
+
+public class 암호해독 {
+
+	public static void main(String[] args) {
+		Solution3 s = new Solution3();
+		System.out.println(s.solution("dfjardstddetckdaccccdegk", 4));
+		System.out.println(s.solution("pfqallllabwaoclk", 2));
+
+	}
+
+}
