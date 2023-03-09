c93d37fa79b076e833bb452bfe4b2082eab23a5f
Wonyoungpark <holly31521@gmail.com>
2023-03-09 18:27:11 +0900

    daily java

---
+++ JAVA/java_study/src/Programmers/daily/java_study/숨어있는숫자의덧셈2.java
@@ -0,0 +1,36 @@
+package Programmers.daily.java_study;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution21 {
+    public int solution(String my_string) {
+        int answer = 0;
+        String str = "";
+        Boolean flag = true;
+        
+        for(char my : my_string.toCharArray()) {
+            if (Character.isDigit(my)) {
+                str+= my;
+                flag = false;
+            }
+            else if (str!="") {
+                answer += Integer.valueOf(str);
+                str = "";
+            }
+        }
+        if (str!="") {
+                answer += Integer.valueOf(str);
+                flag = false;
+        }
+        if (flag==true) return 0;
+        return answer;
+    }
+}
+
+public class 숨어있는숫자의덧셈2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
