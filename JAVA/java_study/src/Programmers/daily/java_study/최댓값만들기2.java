3930d55923026daa05b3d25e4aadd49892acbbc9
Wonyoungpark <holly31521@gmail.com>
2023-03-10 10:45:56 +0900

    daily java

---
+++ JAVA/java_study/src/Programmers/daily/java_study/최댓값만들기2.java
@@ -0,0 +1,24 @@
+package Programmers.daily.java_study;
+
+class Solution22 {
+    public int solution(int[] numbers) {
+        int answer = numbers[0]*numbers[1];
+        
+        for (int i=0;i<numbers.length;i++){
+            for (int j=i+1;j<numbers.length;j++){
+                int num = numbers[i]*numbers[j];
+                if (num>answer) answer = num;
+            }
+        }
+        return answer;
+    }
+}
+
+public final class 최댓값만들기2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
