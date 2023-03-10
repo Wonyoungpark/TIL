53e34b49019a5c55a0e5af8fff7313b8f7165e30
Wonyoungpark <holly31521@gmail.com>
2023-03-10 14:06:03 +0900

    daily java

---
+++ JAVA/java_study/src/Programmers/daily/java_study/각도기.java
@@ -0,0 +1,19 @@
+package Programmers.daily.java_study;
+
+class Solution24 {
+    public int solution(int angle) {
+        if (angle<90) return 1;
+        else if (angle==90) return 2;
+        else if (angle<180) return 3;
+        else return 4;
+    }
+}
+
+public class 각도기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
