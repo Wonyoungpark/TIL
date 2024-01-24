ec6f0ed145c0f6fab0d73bfa4edb313adc5a03c5
holly-21 <holly31521@gmail.com>
2024-01-25 06:54:27 +0900

    add daily java

--- JAVA/ThisisJavaSecondEdition/thisisjava/src/ch06/sec03/SportsCar.java
+++ JAVA/ThisisJavaSecondEdition/thisisjava/src/ch06/sec03/SportsCar.java
@@ -5,5 +5,8 @@ public class SportsCar {
 }
 
 class Tire {
-	
+	String model = "그랜저"; //고유 데이터 필드
+	int speed = 300; // 상태 데이터 필드
+	boolean start = true; //상태 데이터 필드
+	Tire tire = new Tire(); //부품 객체 필드
 }
