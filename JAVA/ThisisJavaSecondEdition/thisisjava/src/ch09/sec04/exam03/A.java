bfbcf921196c855d13b1d65f91d81a22f7f2c4c7
holly-21 <holly31521@gmail.com>
2024-01-31 06:52:11 +0900

    add java til

---
+++ JAVA/ThisisJavaSecondEdition/thisisjava/src/ch09/sec04/exam03/A.java
@@ -0,0 +1,25 @@
+package ch09.sec04.exam03;
+
+public class A {
+	public void method1(final int arg) {
+		final int var = 1;
+		
+		class B{
+			void method2() {
+				System.out.println("arg: "+arg);
+				System.out.println("var:"+var);
+			}
+		}
+		
+//		arg = 2;
+//		var = 2;
+		
+		System.out.println("arg: "+arg);
+		System.out.println("var: "+var);
+	}
+	
+	public static void main(String[] args) {
+		A a = new A();
+		a.method1(3);
+	}
+}
