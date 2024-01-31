20fbf3792d54ca6eea4bfd860c09cf4a8dfd0a1e
holly-21 <holly31521@gmail.com>
2024-01-31 11:47:17 +0900

    p

--- Python/Programmers/두원사이의정수쌍.py
+++ Python/Programmers/두원사이의정수쌍.py
@@ -4,7 +4,7 @@ def solution(r1, r2):
     answer = 0
     
     for i in range(0,r1):
-        #축 한 곳에서 큰원-작원의 개수를 구한다.
+        #축 한 곳에서 큰원-작은원의 개수를 구한다.
         answer += int(sqrt(r2**2-i**2)) - int(sqrt(r1**2-i**2-1))
         
     # 작은원 축 범위 밖의 점 개수
