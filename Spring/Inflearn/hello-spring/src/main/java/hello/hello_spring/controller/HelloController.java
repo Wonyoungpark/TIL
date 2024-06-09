bfd4e855bed92a4c914ca3c9e2f40b9ff31fb252
holly-21 <holly31521@gmail.com>
2024-06-09 15:11:54 +0900

    add spring til

--- Spring/Inflearn/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ Spring/Inflearn/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,12 +3,48 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data","hello!!!");
+        model.addAttribute("data","hello!!!"); //스프링에서 직접 값을 받는다.
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value="name",required = true) String name, Model model){ //외부에서 파라미터를 받는다.
+        model.addAttribute("name",name); //parameter로 넘어온 name을 넘긴다.
+        return "hello-template";
+    }
+
+    // 문자로 보내는 방식
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+ name;
+    }
+
+    // 데이터를 보내는 방식
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); // 객체 생성
+        hello.setName(name); // parameter로 넘어온 name을 넘긴다.
+        return hello; //객체 반환
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
